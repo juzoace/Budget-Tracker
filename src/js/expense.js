71fae67b7cb3d546edc5cf341233ffc3b965b002
Nwigwe Uzochukwu <uzochukwunwigwe@gmail.com>
2020-01-30 19:04:35 +0100

    Remains the budget, income and expense controllers

--- src/js/expense.js
+++ src/js/expense.js
@@ -4,9 +4,15 @@ class Expense {
         // this.description = description;
         // this.value = value;
         this.expenses = [];
+        this.id = [];
     }
 
     updateExpenseDataStructure(input) {
+        
+        // update expense id
+        this.id.push(input.uniqueId);
+        console.log(this.id);
+        // update expense id
         this.expenses.push(input.value);
         
         let convertedExpenses = this.expenses.map(Number);
--- src/js/income.js
+++ src/js/income.js
@@ -4,12 +4,18 @@ class Income {
         // this.description = description;
         // this.value = value;
         this.income = []
+        this.id = [];
     }
 
     updateIncomeDataStructure(input) {
+        
+        // update income id
+        this.id.push(input.uniqueId);
+        console.log(this.id);
+        // update income value
         this.income.push(input.value);
         console.log(this.income);
-        
+        console.log(input.uniqueId);
         //Convert to integer
         let convertedIncome = this.income.map(Number);
         console.log(convertedIncome);
